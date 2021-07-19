e90a41a3d28734f9ebe71f1ebb47cdd048a8bc6c
Harry <harryyork97@gmail.com>
2021-07-19 11:26:19 +0100

    styled the review cards list on the home page

--- src/components/SingleReviewCard/SingleReviewCard.jsx
+++ src/components/SingleReviewCard/SingleReviewCard.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { patchVote } from '../../utils/api'
-import "./SingleReviewCard.css"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { patchVote } from "../../utils/api";
+import "./SingleReviewCard.css";
 
 function SingleReviewCard({ setVoted, isHome, review, reviewBody }) {
-    const review_id = review.review_id
+    const review_id = review.review_id;
 
     return (
         <li className="review-card">
-            {isHome ? <Link to={`/reviews/${review_id}`} ><h3>{review.title}</h3></Link>: <h3>{review.title}</h3>}
+            <section className="review-card__title-owner">
+                {isHome ? (
+                    <Link to={`/reviews/${review_id}`}>
+                        <h3>{review.title}</h3>
+                    </Link>
+                ) : (
+                    <h3>{review.title}</h3>
+                )}
+                <p>OP: {review.owner}</p>
+            </section>
             <p>{reviewBody}</p>
-            <p>Votes: {review.votes}</p>
-            <p>OP: {review.owner}</p>
-            <p>Created on: {new Date(review.created_at).toDateString()}</p>
-            <button onClick={
-                async () => {
-                    await patchVote(review_id, 1)
-                    await setVoted((currVote) =>  currVote + 1)
-                }
-            }>+1 Vote</button>
+            <section className="review-card__votes-date">
+                <p>Created on: {new Date(review.created_at).toDateString()}</p>
+                <section className="review-card__votes">
+                    <p>Votes: {review.votes}</p>
+                    <button
+                        onClick={async () => {
+                            await patchVote(review_id, 1);
+                            await setVoted((currVote) => currVote + 1);
+                        }}
+                    >
+                        +1 Vote
+                    </button>
+                </section>
+            </section>
         </li>
-    )
+    );
 }
 
-export default SingleReviewCard
+export default SingleReviewCard;
